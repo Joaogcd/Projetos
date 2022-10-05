be5f03d90bb2e6189fd2e45bad4ce3f0a858a624
João Guilherme <109766409+Joaogcd@users.noreply.github.com>
2022-10-04 21:58:20 -0300

    att

--- calculadora/script.js
+++
@@ -1,9 +0,0 @@
-function displaynum(n1){
-    calculator.text1.value = calculator.text1.value + n1;
-}
-
-function backspace() {
-    let input = document.getElementById('num');
-    let inputText = input.value;
-    input.value = inputText.substring(0,inputText.length-1);
-}
